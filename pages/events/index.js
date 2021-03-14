e38aee19afbf32746fc924bf834c63be67906bde
promehedi <mmhasan563@gmail.com>
2021-03-14 17:38:13 +0600

    All-Events Page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,24 @@
+import EventList from '../../components/events/EventList'
+import { getAllEvents } from '../../dummy-data'
+
 const EventsPage = () => {
+  const events = getAllEvents()
+
+  if (!events) {
+    return (
+      <center>
+        <h1>Loading...</h1>
+      </center>
+    )
+  }
+
+  if (!events) {
+    return <h1>No Event Found!</h1>
+  }
+
   return (
     <div>
-      <h1>This is EventsPage</h1>
+      <EventList events={events} />
     </div>
   )
 }
