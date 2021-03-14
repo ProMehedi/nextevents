8b122915be997dc17a456e40d8eb00a523d82c8d
promehedi <mmhasan563@gmail.com>
2021-03-14 17:05:12 +0600

    Event Details Data & Styles

---
+++ components/events/EventDetails.js
@@ -0,0 +1,53 @@
+import styles from '../../styles/EventDetails.module.css'
+import AddressIcon from '../icons/AddressIcon'
+import DateIcon from '../icons/DateIcon'
+
+const EventDetails = ({
+  id,
+  title,
+  date,
+  desc,
+  image,
+  isFeatured,
+  location,
+}) => {
+  const readableDate = new Date(date).toLocaleDateString('en-us', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
+
+  const formatedAddress = location.replace(', ', '\n')
+
+  return (
+    <>
+      <div className={styles.summary}>
+        <h1>{title}</h1>
+      </div>
+      <section className={styles.logistics}>
+        <div className={styles.image}>
+          <img src={`/${image}`} alt={title} />
+        </div>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <span className={styles.icon}>
+              <DateIcon />
+            </span>
+            {readableDate}
+          </li>
+          <li className={styles.item}>
+            <span className={styles.icon}>
+              <AddressIcon />
+            </span>
+            <address>{formatedAddress}</address>
+          </li>
+        </ul>
+      </section>
+      <div className={styles.content}>
+        <p>{desc}</p>
+      </div>
+    </>
+  )
+}
+
+export default EventDetails
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import EventDetails from '../../components/events/EventDetails'
+import { getEventById } from '../../dummy-data'
 
-const SingleEventPage = () => {
+const SingleEventsPage = () => {
+  const router = useRouter()
+  const { eventId } = router.query
+  const event = getEventById(eventId)
+  const { id, title, date, description, image, isFeatured, location } = event
+
+  console.log(event)
+
+  if (!event) {
+    return <h1>No Event Found!</h1>
+  }
+
   return (
     <div>
-      <h1>This is SingleEventPage</h1>
+      <EventDetails
+        title={title}
+        desc={description}
+        image={image}
+        location={location}
+        date={date}
+      />
     </div>
   )
 }
 
-export default SingleEventPage
+export default SingleEventsPage
